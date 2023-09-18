a8fb47b88ada20d8771b7c40c5fe527034fcba7e
nishan4477 <104998929+nishan4477@users.noreply.github.com>
2023-09-18 17:04:14 +0545

    upto experience done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,65 @@
+
 import React from 'react'
 import "./about.scss"
+import { Col, Container, Row } from 'react-bootstrap'
+import IMG from '../../assets/aboutimg.jpg'
+import {SiJsonwebtokens} from 'react-icons/si'
+
 
 const About = () => {
   return (
-<section id='about'>About
+<section id='about' >
+  <h5>Get To Know</h5>
+  <h2>About Me</h2>
+
+  <Container className='pt-3'> 
+  <Row>
+    <Col lg='5' sm='5' xs='5'>
+      <div className="about__img">
+      <img className=' border rounded' src={IMG} alt="" />
+
+
+      </div>
+     
+
+    
+    </Col>
+
+    <Col lg='7'>
+    <Row className='gap-5'>
+      <Col className='about__card text-center ' lg='3'>
+      <h2 className='mb-3'><SiJsonwebtokens/></h2>
+      <h3>Experience</h3>
+      <p>1+ year</p>
+        
+      </Col>
+      <Col className='about__card text-center' lg='3'>
+      <h2 className='mb-3'><SiJsonwebtokens/></h2>
+      <h3>Client</h3>
+      <p>10+</p>
+        
+      </Col>
+      <Col className='about__card text-center' lg='3'>
+      <h2 className='mb-3'><SiJsonwebtokens/></h2>
+      <h3>Project</h3>
+      <p>50+</p>
+        
+      </Col>
+
+
+    </Row>
+    <Row>
+      <p className='mt-5 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fuga atque assumenda excepturi quo provident modi quia suscipit iusto, eius fugit eaque doloribus minima dolorum aperiam, enim saepe laboriosam voluptas?Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim cupiditate odio sit ab beatae fugit, rem quasi possimus, et tempora officiis ipsum maiores incidunt! Odit optio iure quo alias non.</p>
+    </Row>
+    </Col>
+
+
+
+  </Row>
+
+
+    
+  </Container>
 
 </section>
   )
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,123 @@
-import React from 'react'
-import './experience.scss'
-
+import React from "react";
+import "./experience.scss";
+import { Col, Container, Row } from "react-bootstrap";
+import {GiCheckMark} from 'react-icons/gi'
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
-}
+    <section id="experience" className="mt-4">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <Container className="pt-3">
+        <Row >
+          <Col lg='5' className="ex__card">
+          <div className="pb-2 text-center"><h2>Frontend Development</h2></div> 
+            <Row>
+              <Col>
+            
+              <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>HTML</h4>
+                 <p>Experienced</p>
+                </Col>
+              </Row>
+              
+            
+              </Col>
 
-export default Experience
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>CSS</h4>
+                 <p>Intermediate</p>
+                </Col>
+              </Row></Col>
+            </Row>
+            <Row>
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>Javascript</h4>
+                 <p>Intermediate</p>
+                </Col>
+              </Row></Col>
+
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>Bootstrap</h4>
+                 <p>Experienced</p>
+                </Col>
+              </Row></Col>
+            </Row>
+            <Row>
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>Tailwind</h4>
+                 <p>Intermediate</p>
+                </Col>
+              </Row></Col>
+
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>React</h4>
+                 <p>Experienced</p>
+                </Col>
+              </Row></Col>
+            </Row>
+          </Col>
+
+          <Col lg='5' className="ex__card">
+          <div className="pb-2 text-center"><h2>Backend Development</h2></div> 
+         
+          <Row>
+              <Col> 
+            
+               <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>Node JS</h4>
+                 <p>Beginner</p>
+                </Col>
+              </Row></Col>
+
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>Mongo DB</h4>
+                 <p>None</p>
+                </Col>
+              </Row></Col>
+            </Row>
+            <Row>
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>PHP</h4>
+                 <p>Basic</p>
+                </Col>
+              </Row></Col>
+
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>Python</h4>
+                 <p>Basic</p>
+                </Col>
+              </Row></Col>
+            </Row>
+            <Row>
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>MYSQL</h4>
+                 <p>Basic</p>
+                </Col>
+              </Row></Col>
+
+              <Col>  <Row>
+                <Col><GiCheckMark/></Col>
+                <Col><h4>HTML</h4>
+                 <p>Experienced</p>
+                </Col>
+              </Row></Col>
+            </Row>
+            </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Experience;
